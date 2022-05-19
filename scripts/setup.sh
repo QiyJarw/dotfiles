75080ddb33d6e01643b28eb39fcfd818cc1df81f
QiyJarw <m1t2t3ch@gmail.com>
2022-05-19 10:15:54 +0900

    update: add a description.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,4 +10,5 @@ source ${dotfiles_root}/scripts/install.sh # brew及びツールのインスト�
 
 echo "==================================================================================================="
 printf "\033[1;32msetup finished successfully!\033[0;39m\n"
-printf "current shell is ${SHELL}. If you change the shell, you need to type command: \033[1;33m'which zsh | chsh -s'\033[0;39m, and reboot terminal.\n"
+printf "Current shell is ${SHELL}. If you change the shell, you need to type command: \033[1;33m'which zsh | chsh -s'\033[0;39m, and reboot terminal.\n"
+printf "Please type command: \33[1;33m'yarn config set global-path '/home/user-name/.local/bin'\33[0;39m to change a global path of yarn.\n"
